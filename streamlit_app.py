497f71094d1cf7414633f6d1acf7111522b24faf
Kankan Saikia <136485606+Kankansaikia@users.noreply.github.com>
2026-04-01 17:17:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ values ('""" + ingredients_string + """','""" + name_on_order + """')
 
 # New section to display smoothiefroot nutrition information
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
